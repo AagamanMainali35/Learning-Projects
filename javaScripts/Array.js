ccf78b162c66d85db1051417a36c02df9f10b3e3
aagaman <aagamanmainali35@gmail.com>
2024-07-11 18:17:30 +0545

    Update Array.js

--- javaScripts/Array.js
+++ javaScripts/Array.js
@@ -1,1 +1,6 @@
-let fruits=['apple']
+let fruits=['apple','banana','orange']
+// console.log(fruits[0],fruits[1],fruits[2]) Accesing the elemnts with index 
+// fruits.pop() deletes the last index of the array
+// fruits.sort() sorts them alpahbetically 
+// fruits.reverse() reverses the array in current order
+//console.log(fruits.length) returm the length of the array 

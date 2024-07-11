4cdd47d0a1bdeb1bb367f5154bde10e155c8344a
aagaman <aagamanmainali35@gmail.com>
2024-07-11 17:33:55 +0545

    NewFilesAdded
    JavaScripts for learniing purpose

---
+++ javaScripts/functons.js
@@ -0,0 +1,7 @@
+function age(x,y){
+    result=x+y;
+    // console.log(result)//Console.log or print use garyo re maile function lai vane maile tesko result lai further use garna mildaina arko v
+     return result;
+}
+square=Math.sqrt(age(1,2))
+console.log(square)//cant transform result without return 
